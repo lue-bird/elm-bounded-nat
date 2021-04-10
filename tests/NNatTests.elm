f1e621cc812fda7af4e4f4a5831e6181e2ec1008
passi <pascalfeyer@gmail.com>
2021-04-10 18:36:00 +0200

    +- all tests in describe NNat

--- tests/NNatTests.elm
+++ tests/NNatTests.elm
@@ -3,6 +3,7 @@ module NNatTests exposing (suite)
 {-| Especially type tests.
 -}
 
+import Dict exposing (Dict)
 import Expect
 import NNat
 import NNats exposing (..)
@@ -14,13 +15,15 @@ import TypeNats exposing (..)
 
 suite : Test
 suite =
-    test "add x |> sub x stays the same"
-        (\() ->
-            nat11
-                |> NNat.add ( nat9, nat9 )
-                |> NNat.sub ( nat9, nat9 )
-                |> Expect.equal nat11
-        )
+    describe "NNat"
+        [ test "add x |> sub x stays the same"
+            (\() ->
+                nat11
+                    |> NNat.add ( nat9, nat9 )
+                    |> NNat.sub ( nat9, nat9 )
+                    |> Expect.equal nat11
+            )
+        ]
 
 
 testAddN =
