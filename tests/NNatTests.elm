37dabea44709c2f5ae685f2a67431154c26263cf
passi <pascalfeyer@gmail.com>
2021-08-08 09:37:15 +0200

    + test big

--- tests/NNatTests.elm
+++ tests/NNatTests.elm
@@ -1,35 +1,88 @@
 module NNatTests exposing (suite)
 
-{-| Especially type tests.
--}
-
-import Dict exposing (Dict)
 import Expect
 import NNat
-import NNats exposing (..)
 import Nat exposing (Is, N, Nat, To)
+import Nats exposing (..)
 import Test exposing (Test, describe, test)
-import TypeNats exposing (..)
 
 
 suite : Test
 suite =
     describe "NNat"
-        [ test "add x |> sub x stays the same"
+        [ test "add & sub"
             (\() ->
                 nat11
                     |> NNat.add ( nat9, nat9 )
+                    |> Expect.equal nat20
+            )
+        , test "sub"
+            (\() ->
+                nat11
                     |> NNat.sub ( nat9, nat9 )
-                    |> Expect.equal nat11
+                    |> Expect.equal nat2
             )
         ]
 
 
-testAdd : Nat (N Nat16 (Nat16Plus more_) (Is a To (Nat16Plus a)) (Is b To (Nat16Plus b)))
-testAdd =
-    nat7 |> NNat.add ( nat9, nat9 )
+test0 :
+    Nat
+        (N
+            (Nat160Plus Nat160)
+            (Nat160Plus (Nat160Plus a_))
+            (Is a To (Nat160Plus (Nat160Plus a)))
+            (Is b To (Nat160Plus (Nat160Plus b)))
+        )
+test0 =
+    nat160 |> NNat.add ( nat160, nat160 )
 
 
-testSub : Nat (N Nat8 (Nat8Plus more_) (Is a To (Nat8Plus a)) (Is b To (Nat8Plus b)))
-testSub =
-    nat17 |> NNat.sub ( nat9, nat9 )
+test1 :
+    Nat
+        (N
+            (Nat160Plus Nat160)
+            (Nat160Plus (Nat160Plus a_))
+            (Is a To (Nat160Plus (Nat160Plus a)))
+            (Is b To (Nat160Plus (Nat160Plus b)))
+        )
+test1 =
+    nat160 |> NNat.add ( nat160, nat160 )
+
+
+test2 :
+    Nat
+        (N
+            (Nat160Plus Nat160)
+            (Nat160Plus (Nat160Plus a_))
+            (Is a To (Nat160Plus (Nat160Plus a)))
+            (Is b To (Nat160Plus (Nat160Plus b)))
+        )
+test2 =
+    nat160 |> NNat.add ( nat160, nat160 )
+
+
+test3 :
+    Nat
+        (N
+            (Nat160Plus Nat159)
+            (Nat160Plus (Nat159Plus a_))
+            (Is a To (Nat160Plus (Nat159Plus a)))
+            (Is b To (Nat160Plus (Nat159Plus b)))
+        )
+test3 =
+    nat160 |> NNat.add ( nat159, nat159 )
+
+
+testBig :
+    Nat
+        (N
+            (Nat160Plus (Nat159Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus Nat160)))))))
+            (Nat160Plus (Nat159Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus a_))))))))
+            (Is a To (Nat160Plus (Nat159Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus a)))))))))
+            (Is b To (Nat160Plus (Nat159Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus (Nat160Plus b)))))))))
+        )
+testBig =
+    test0
+        |> NNat.add ( test1, test1 )
+        |> NNat.add ( test2, test2 )
+        |> NNat.add ( test3, test3 )
